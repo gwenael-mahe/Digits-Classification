51874a182301833a7af6bfe56ba1b3c88d5ec3b7
Gwénaël <gwenael.mahe@laplateforme.io>
2022-11-17 14:51:22 +0100

    push flask + notebook

--- Application Flask MNIST/App.py
+++ Application Flask MNIST/App.py
@@ -1,34 +1,70 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from skimage.transform import resize
+import imageio.v3 as iio
+import numpy as np
+import re
+import base64
 
 from fonction import predict
 
 app = Flask(__name__)
-app.config["SEND_FILE_MAX_AGE_DEFAULT"]=1
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
+
 
 @app.route("/")
 def index():
-    a= 25 
-    b="La deep nous a quitter "
-
-    return render_template("index.html", age=a, déclaration=b)
+    return render_template("index.html")
 
 
-@app.route("/Formulaire")
+@app.route("/Upload")
 def formulaire():
-    a= 25 
-    b="La deep nous a quitter "
+    return render_template("formulaire.html")
 
-    return render_template("formulaire.html", age=a, déclaration=b)
 
-@app.route("/Formulaire/reponse", methods = ["post"])
+@app.route("/Formulaire/reponse", methods=["post"])
 def fichier():
-    u=request.files["iphot"]
-    t=predict(u)
-    
+    u = request.files["iphot"]
+    t = predict(u)
+
     print(t)
 
-    return render_template ("formulaire.html", v=t)
+    return render_template("formulaire.html", v=t)
 
 
-if __name__ == "__main__" :
-    app.run(debug =True)
+@app.route("/Dessin")
+def dessin():
+    return render_template("dessin.html")
+
+
+@app.route('/predict/', methods=['GET', 'POST'])
+def predict_dessin():
+    # get data from drawing canvas and save as image
+    parseImage(request.get_data())
+
+    # read parsed image back in 8-bit, black and white mode (L)
+    x = iio.imread('output.png', mode='L')
+    x = np.invert(x)
+    x = resize(x, (28, 28))
+
+    # reshape image data for use in neural network
+    x = x.reshape(1, 28, 28, 1)
+
+    model = load_model("model.h5")
+
+    # with graph.as_default():
+    out = model.predict(x)
+    print(out)
+    print(np.argmax(out, axis=1))
+    response = np.array_str(np.argmax(out, axis=1))
+    return response
+
+
+def parseImage(imgData):
+    # parse canvas bytes and save as output.png
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    with open('output.png', 'wb') as output:
+        output.write(base64.decodebytes(imgstr))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Application Flask MNIST/fonction.py
+++ Application Flask MNIST/fonction.py
@@ -1,10 +1,8 @@
 from tensorflow.keras.models import load_model
 from PIL import Image
-# from utils import imread, imresize
-import imageio
-# imageio.imread("xxxx.png")
 import numpy as np
-model = load_model("model_hand.h5")
+
+model = load_model("model.h5")
 
 
 def predict(image):
@@ -17,16 +15,9 @@ def predict(image):
     x_reshaped = x_np.reshape(((1,28,28,1)))
 
     print('b')
-    # print(x.shape)
-    # reshape image data for use in neural network
-    # x = x.reshape(1,28,28,1)
     out = model.predict(x_reshaped)
     print("fin")
     print(out)
     print(np.argmax(out, axis=1))
-    
-    
-
-    # resultat=model.predict([])
 
     return np.argmax(out, axis=1)
